51818d6519af091e4d7f7bc7253d912ce5ff733f
Aditya Patel <123244634+iampateladitya@users.noreply.github.com>
2024-01-19 21:54:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ function checkName() {
 
     if (name.length === 7) {
       resultDiv.innerHTML = '<h1>Thala for a reason!</h1>' +
-        '<video width="300" height="300" autoplay>' +
+        '<video width="400" height="400" autoplay>' +
           '<source src="' + videoPathThala + '" type="video/mp4">' +
           'Your browser does not support the video tag.' +
         '</video>';
     } else {
       resultDiv.innerHTML = '<h1>Not a Thala!</h1>' +
-        '<video width="300" height="300" autoplay>' +
+        '<video width="400" height="400" autoplay>' +
           '<source src="' + videoPathNotThala + '" type="video/mp4">' +
           'Your browser does not support the video tag.' +
         '</video>';
